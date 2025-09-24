fba4fcf9aca53b41f43a6df4b261eced9880c9f7
Nico26x <nicolas.loaizab@uqvirtual.edu.co>
2025-09-24 07:51:37 -0500

    correccion nodo simple

--- src/listasListasimple/Nodo.java
+++ src/listasListasimple/Nodo.java
@@ -1,22 +1,13 @@
 package listasListasimple;
 
-public class Nodo<T> { 
-   private T dato;
+public class Nodo<T> {
+
+    private T dato;
 
     private Nodo<T> siguiente;
 
     public Nodo(T dato) {
         this.dato = dato;
-    } 
-    public T getDato() {
-        return dato;
-    }
-
-    public void setDato(T dato) {
-        this.dato = dato;
-    }
-    public Nodo<T> getSiguiente() {
-        return siguiente;
     }
 
     public T getDato() {
@@ -29,10 +20,6 @@ public class Nodo<T> {
 
     public Nodo<T> getSiguiente() {
         return siguiente;
-    }
-
-    public void setSiguiente(Nodo<T> siguiente) {
-        this.siguiente = siguiente;
     }
 
 }
